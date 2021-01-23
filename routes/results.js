3764eadc7b338c86384bc75c24f29956e60f5edf
wkoslik <wkoslik@gmail.com>
2021-01-23 09:30:50 -0500

    only showing results based on user preference. userPref currently hard coded. fixing food names to have first letter capitalized only

--- routes/results.js
+++ routes/results.js
@@ -9,6 +9,7 @@ let foodDatabase = `https://api.edamam.com/api/food-database/v2/parser?ingr=${se
 let nutrients = `https://api.edamam.com/api/food-database/v2/nutrients?app_id=${process.env.INGREDIENT_ID}&app_key=${process.env.INGREDIENT_KEY}`;
 let recipeDatabase = `https://api.edamam.com/search?q=${searchTerm}&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}&health=vegan&health=vegetarian`
 
+let userPreference = 'health=vegan'
 
 router.get('/', (req, res) => {
     searchTerm = req.query.searchterm;
@@ -16,36 +17,36 @@ router.get('/', (req, res) => {
     let database = req.query.searchtype;
     console.log(database);
     if (database == 'food') { //IF SEARCHING FOR INGREDIENT
-        axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${searchTerm}&app_id=${process.env.INGREDIENT_ID}&app_key=${process.env.INGREDIENT_KEY}`)
+        axios.get(`https://api.edamam.com/api/food-database/v2/parser?ingr=${searchTerm}&app_id=${process.env.INGREDIENT_ID}&app_key=${process.env.INGREDIENT_KEY}&${userPreference}`)
             .then((response) => {
                 let foodResults = response.data.hints;
-                let ingObject = {
-                    "ingredients": [
-                        {
-                            "quantity": 1,
-                            "measureURI": foodResults[0].measures[8].uri,
-                            "foodId": foodResults[0].food.foodId
-                        }
-                    ]
-                }
-
-                foodResults.forEach(food => {
-                    //BLOWS OUT THE LIMITS, need to figure out workaround
-                    axios.post(`https://api.edamam.com/api/food-database/v2/nutrients?app_id=${process.env.INGREDIENT_ID}&app_key=${process.env.INGREDIENT_KEY}`, {
-                        "ingredients": [
-                            {
-                                "quantity": 1,
-                                "measureURI": food.measures[0].uri,
-                                "foodId": food.food.foodId
-                            }
-                        ]
-                    }
-                    )
-                        .then(response => {
-                            res.send(response.data);
-                        })
-                }
-                )
+                // let ingObject = {
+                //     "ingredients": [
+                //         {
+                //             "quantity": 1,
+                //             "measureURI": foodResults[0].measures[8].uri,
+                //             "foodId": foodResults[0].food.foodId
+                //         }
+                //     ]
+                // }
+                res.render('results', { foodResults });
+                // foodResults.forEach(food => {
+                //     //BLOWS OUT THE LIMITS, need to figure out workaround
+                //     axios.post(`https://api.edamam.com/api/food-database/v2/nutrients?app_id=${process.env.INGREDIENT_ID}&app_key=${process.env.INGREDIENT_KEY}`, {
+                //         "ingredients": [
+                //             {
+                //                 "quantity": 1,
+                //                 "measureURI": food.measures[0].uri,
+                //                 "foodId": food.food.foodId
+                //             }
+                //         ]
+                //     }
+                //     )
+                // .then(response => {
+                //     res.send(response.data);
+                // })
+                // }
+                // )
             })
     } else { //IF SEARCHING FOR RECIPE
         axios.get(`https://api.edamam.com/search?q=${searchTerm}&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}&health=vegan&health=vegetarian`)
