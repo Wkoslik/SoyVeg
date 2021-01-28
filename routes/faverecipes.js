f12b00fef66079307ddfbfe323e143e6fd3cc004
wkoslik <wkoslik@gmail.com>
2021-01-28 14:31:55 -0500

    cleaning code in faverecipes.js and adding comments

--- routes/faverecipes.js
+++ routes/faverecipes.js
@@ -1,8 +1,3 @@
-//TODO remove res.send
-//TODO remove console.logs
-//TODO add comments as to what the routes are doing
-//TODO add comments as to why the routes are doing what theyre doing
-
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
@@ -11,10 +6,8 @@ const db = require('../models');
 
 //load favorite recipes
 router.get('/', (req, res) => {
-    //show all liked recipes where userId = req.user.id
     db.user.findByPk(req.user.id).then(user => {
         user.getLikedrecipes().then(recipe => {
-            //res.send(recipe);
             res.render('fave/faverecipes', { recipe })
         })
     })
@@ -22,7 +15,6 @@ router.get('/', (req, res) => {
 
 //add favorite recipe to db
 router.post('/', (req, res) => {
-    //res.send(req.body);
     let fullIngredients = req.body.ingredients;
     let length = 250;
     let trimmedIngrientsList = fullIngredients.substring(0, length);
@@ -36,9 +28,6 @@ router.post('/', (req, res) => {
     }).then(([recipe, created]) => {
         db.user.findByPk(req.user.id).then(user => {
             recipe.addUser(user).then(relationship => {
-                //console.log(`${ingredient.name} was liked by ${user.name}`);
-                //res.send(`${ingredient.name} was liked by ${user.name}`);
-                //res.send('CREATED?')
                 res.redirect('/faverecipes');
             })
         })
@@ -51,7 +40,6 @@ router.delete('/:id', (req, res) => {
     db.user.findByPk(req.user.id).then(user => {
         db.likedrecipe.findByPk(req.body.id).then(recipe => {
             user.removeLikedrecipe(recipe).then(removed => {
-                //             console.log("REMOVED!!!")
                 res.redirect('/faverecipes');
             })
         })
